98c9aac2c2a74ae7c3c82913abd878418ac720ff
zziro95 <opers7878@gmail.com>
2021-01-18 23:43:50 +0900

    Feat: Deployment target에 관련된 오류 해결을 위한 annotation 코드 추가

--- WeatherForecast/WeatherForecast/SceneDelegate.swift
+++ WeatherForecast/WeatherForecast/SceneDelegate.swift
@@ -1,15 +1,9 @@
-//
-//  WeatherForecast - SceneDelegate.swift
-//  Created by yagom. 
-//  Copyright © yagom. All rights reserved.
-// 
-
 import UIKit
 
+@available(iOS 13.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -45,7 +39,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
 
