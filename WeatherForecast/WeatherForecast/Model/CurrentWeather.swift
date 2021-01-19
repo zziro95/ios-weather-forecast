32eb31d2ec55bf68515596f31a2698bbb7417f18
zziro95 <opers7878@gmail.com>
2021-01-19 15:28:20 +0900

    Feat: CurrentWeather 현재날씨 데이터 받아오기 위한 타입 선언

---
+++ WeatherForecast/WeatherForecast/Model/CurrentWeather.swift
@@ -0,0 +1,62 @@
+import Foundation
+
+struct CurrentWeather: Codable {
+    let coord: Coord
+    let weather: [Weather]
+    let base: String
+    let main: Main
+    let wind: Wind
+    let clouds: Clouds
+    let dt: Int
+    let sys: Sys
+    let timezone: Int
+    let id: Int
+    let name: String
+    let cod: Int
+    
+    struct Coord: Codable {
+        let lon: Double
+        let lat: Double
+    }
+    
+    struct Weather: Codable {
+        let id: Int
+        let main: String
+        let description: String
+        let icon: String
+    }
+    
+    struct Main: Codable {
+        let temp: Double
+        let feelsLike: Double
+        let tempMin: Double
+        let tempMax: Double
+        let pressure: Int
+        let humidity: Int
+        
+        enum CodingKeys: String, CodingKey {
+            case temp, pressure, humidity
+            case feelsLike = "feels_like"
+            case tempMin = "temp_min"
+            case tempMax = "temp_max"
+        }
+    }
+    
+    struct Wind: Codable {
+        let speed: Double
+        let deg: Int
+    }
+    
+    struct Clouds: Codable {
+        let all: Int
+    }
+    
+    struct Sys: Codable {
+        let type: Int
+        let id: Int
+        let message: Double
+        let country: String
+        let sunrise: Int
+        let sunset: Int
+    }
+}
